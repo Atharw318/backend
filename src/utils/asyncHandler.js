fcdb0b96e028a19343a68014e9de767d1aebaa55
Santosh <atharw010@gmail.com>
2024-04-08 21:58:42 +0530

    all setup cinfiger

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,6 @@
-import { Promise } from "mongoose";
-
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
-      next(err);
-    });
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
