99dda04dcfe2cd4e67ed4f72e415605776d0184e
Santosh <atharw010@gmail.com>
2024-04-10 21:03:03 +0530

    add refresh token end point

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 
 const router = Router();
@@ -26,7 +27,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 //secured routes
-
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
